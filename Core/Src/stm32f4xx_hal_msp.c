ce9545432b32822139d2c5cb596344fc16e54328
Rushikesh K <kamblerushikesh8310@gmail.com>
2024-10-20 23:35:31 +0530

    ToDo- Blocking Task Delay

--- Core/Src/stm32f4xx_hal_msp.c
+++ Core/Src/stm32f4xx_hal_msp.c
@@ -71,7 +71,7 @@ void HAL_MspInit(void)
   __HAL_RCC_PWR_CLK_ENABLE();
 
   /* System interrupt init*/
-//  vInitPrioGroupValue();
+ vInitPrioGroupValue();
   /* USER CODE BEGIN MspInit 1 */
 
   /* USER CODE END MspInit 1 */
