4fb7048b00c18dd88431366d0185e6ef3ffd91d6
Rushikesh K <kamblerushikesh8310@gmail.com>
2025-05-12 15:07:09 +0530

    Round Robin Schedular

---
+++ Debug/driver/Device/ST/ST/stm32f4xx/Source/system_stm32f4xx.d
@@ -0,0 +1,16 @@
+driver/Device/ST/ST/stm32f4xx/Source/system_stm32f4xx.o: \
+ ../driver/Device/ST/ST/stm32f4xx/Source/system_stm32f4xx.c \
+ E:/Github/git_repository/FreeRTOS_Kernal/RTOS_Schedular/Round_Robin_Scheduler/driver/Device/ST/ST/stm32f4xx/Include/stm32f4xx.h \
+ E:/Github/git_repository/FreeRTOS_Kernal/RTOS_Schedular/Round_Robin_Scheduler/driver/CMSIS/Core/Include/core_cm4.h \
+ E:/Github/git_repository/FreeRTOS_Kernal/RTOS_Schedular/Round_Robin_Scheduler/driver/CMSIS/Core/Include/cmsis_version.h \
+ E:/Github/git_repository/FreeRTOS_Kernal/RTOS_Schedular/Round_Robin_Scheduler/driver/CMSIS/Core/Include/cmsis_compiler.h \
+ E:/Github/git_repository/FreeRTOS_Kernal/RTOS_Schedular/Round_Robin_Scheduler/driver/CMSIS/Core/Include/cmsis_gcc.h \
+ E:/Github/git_repository/FreeRTOS_Kernal/RTOS_Schedular/Round_Robin_Scheduler/driver/CMSIS/Core/Include/mpu_armv7.h \
+ E:/Github/git_repository/FreeRTOS_Kernal/RTOS_Schedular/Round_Robin_Scheduler/driver/Device/ST/ST/stm32f4xx/Include/system_stm32f4xx.h
+E:/Github/git_repository/FreeRTOS_Kernal/RTOS_Schedular/Round_Robin_Scheduler/driver/Device/ST/ST/stm32f4xx/Include/stm32f4xx.h:
+E:/Github/git_repository/FreeRTOS_Kernal/RTOS_Schedular/Round_Robin_Scheduler/driver/CMSIS/Core/Include/core_cm4.h:
+E:/Github/git_repository/FreeRTOS_Kernal/RTOS_Schedular/Round_Robin_Scheduler/driver/CMSIS/Core/Include/cmsis_version.h:
+E:/Github/git_repository/FreeRTOS_Kernal/RTOS_Schedular/Round_Robin_Scheduler/driver/CMSIS/Core/Include/cmsis_compiler.h:
+E:/Github/git_repository/FreeRTOS_Kernal/RTOS_Schedular/Round_Robin_Scheduler/driver/CMSIS/Core/Include/cmsis_gcc.h:
+E:/Github/git_repository/FreeRTOS_Kernal/RTOS_Schedular/Round_Robin_Scheduler/driver/CMSIS/Core/Include/mpu_armv7.h:
+E:/Github/git_repository/FreeRTOS_Kernal/RTOS_Schedular/Round_Robin_Scheduler/driver/Device/ST/ST/stm32f4xx/Include/system_stm32f4xx.h:
--- Debug/sources.mk
+++ Debug/sources.mk
@@ -22,15 +22,7 @@ C_DEPS :=
 
 # Every subdirectory with source files must be described here
 SUBDIRS := \
-Core/Src \
-Core/Startup \
-Drivers/STM32F4xx_HAL_Driver/Src \
-common/ThirdParty/FreeRTOS \
-common/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F \
-common/ThirdParty/FreeRTOS/portable/MemMang \
-common/ThirdParty/SEGGER/Config \
-common/ThirdParty/SEGGER/OS \
-common/ThirdParty/SEGGER/Rec \
-common/ThirdParty/SEGGER/SEGGER \
-common/ThirdParty/SEGGER/SEGGER/Syscalls \
+Src \
+Startup \
+driver/Device/ST/ST/stm32f4xx/Source \
 
